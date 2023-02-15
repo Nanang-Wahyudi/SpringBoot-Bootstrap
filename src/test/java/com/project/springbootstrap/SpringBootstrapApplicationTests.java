cc5866c1cb259f665e281bdc176997d10f052d23
Diki Center <dikimistak@gmail.com>
2023-02-15 07:25:32 +0700

    Commit : Configurasi SpringBoot Dan Bootstrap

---
+++ src/test/java/com/project/springbootstrap/SpringBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
